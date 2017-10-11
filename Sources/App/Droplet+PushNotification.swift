c293c1c543062d8a9a8f0d942685e20980e6522b
Genki Mine <gomfucius@gmail.com>
2017-10-10 22:59:30 -0700

    Try using dispatch

--- Sources/App/Droplet+PushNotification.swift
+++ Sources/App/Droplet+PushNotification.swift
@@ -1,5 +1,6 @@
 import Vapor
 import VaporAPNS
+import Dispatch
 
 private var _pushNotificationsController: PushNotificationsController!
 
@@ -14,12 +15,34 @@ extension Droplet {
         get("/test", String.parameter) { [weak self] request in
             let deviceToken = try request.parameters.next(String.self)
 
-            self?.pnController.test(withToken: deviceToken) { string in
-
+            let anotherQueue = DispatchQueue(label: "com.summitisland.dungeons", qos: .utility)
+            anotherQueue.async {
+                self?.pnController.test(withToken: deviceToken) { string in
+                }
             }
 
             var json = JSON()
             try json.set("hello", deviceToken)
+            return json
+        }
+
+        get("test-dispatch") { request in
+            let group = DispatchGroup()
+            group.enter()
+
+            var number = 0
+
+            DispatchQueue.global(qos: .background).async {
+                for _ in 0...88888888 {
+                    number += 1
+                }
+                group.leave()
+            }
+
+            group.wait()
+
+            var json = JSON()
+            try json.set("number1", number)
             return json
         }
     }
